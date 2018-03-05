72bbcddcc60f8b680604a650c232b3ef39eeed11
Paul PRIKAZSKY <=>
2018-03-05 14:53:56 +0100

    Correction à la compilation de ft_param_delarr.c

--- ft_ls/srcs/params/ft_param_delarr.c
+++ ft_ls/srcs/params/ft_param_delarr.c
@@ -6,7 +6,7 @@
 /*   By: pprikazs <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 13:01:37 by pprikazs          #+#    #+#             */
-/*   Updated: 2018/03/05 13:06:48 by pprikazs         ###   ########.fr       */
+/*   Updated: 2018/03/05 14:50:58 by pprikazs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,17 @@
 
 extern void		ft_param_delarr(t_param **param)
 {
-	int			i;
+	size_t		i;
 
-	if (param == 0)
-		return (0);
-	i = 0;
-	while (i < FT_PARAM_NBMAX)
+	if (param != 0)
 	{
-		(*param)[i].key = 0;
-		(*param)[i].val = 0;
-		i++;
+		i = 0;
+		while (i < FT_PARAM_NBMAX)
+		{
+			(*param)[i].key = 0;
+			(*param)[i].val = 0;
+			i++;
+		}
+		ft_memdel((void **)param);
 	}
-	ft_memdel((void **)param);	
 }
