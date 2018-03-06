280805e7a4b8edaa2f540573be9a61890178e444
Paul PRIKAZSKY <=>
2018-03-06 19:09:03 +0100

    Dépalcement des test effectué dans ft_ls.c vers ./demo/main.c

--- ft_ls/srcs/ft_ls.c
+++ ft_ls/srcs/ft_ls.c
@@ -6,9 +6,13 @@
 /*   By: pprikazs <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 11:04:54 by pprikazs          #+#    #+#             */
-/*   Updated: 2018/03/05 13:01:04 by pprikazs         ###   ########.fr       */
+/*   Updated: 2018/03/05 18:31:51 by pprikazs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
 
 #include "libft.h"
 #include "ft_ls.h"
@@ -16,10 +20,28 @@
 extern int			ft_ls(int argc, char **argv)
 {
 	t_param			*param;
+	int				cur;
 
 	param = 0;
-	if ((ft_param_parse(argc, argv, &param)) == -1)
+	if ((cur = ft_param_parse(argc, argv, &param)) == -1)
 		return (-1);
-	ft_param_display(param, 0);
+	{
+		DIR				*test;
+		struct dirent	*dir;
+		struct stat		buff;
+	
+		if (!(test = opendir(".")))
+		{
+			ft_putendl("couldn't open .");
+			return (0);
+		}
+		while ((dir = readdir(test)) != 0)
+		{
+			ft_putendl(dir->d_name);
+			stat(dir->d_name, &buff);
+			ft_putnbr(buff.st_blocks);
+		}
+	}
+//	ft_param_display(param, 0);
 	return (0);
 }
