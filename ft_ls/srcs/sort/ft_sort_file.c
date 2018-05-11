f96540dbd3a1d6e4ee0c8fd6dd3a784fbda1c48d
Paul PRIKAZSKY <paul.prikazsky@gmail.com>
2018-05-11 19:53:17 +0200

    Ajout de l'option -t

--- ft_ls/srcs/sort/ft_sort_file.c
+++ ft_ls/srcs/sort/ft_sort_file.c
@@ -6,7 +6,7 @@
 /*   By: pprikazs <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/10 12:13:26 by pprikazs          #+#    #+#             */
-/*   Updated: 2018/05/11 18:36:08 by pprikazs         ###   ########.fr       */
+/*   Updated: 2018/05/11 19:51:48 by pprikazs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ extern void			ft_sort_file(t_file *files, size_t size)
 {
 	if (ft_param_get('r'))
 		ft_qsort((void *)files, size, sizeof(t_file), &ft_sort_filecmp_d); 
-	else
+	else if (ft_param_get('t') == 0)
 		ft_qsort((void *)files, size, sizeof(t_file), &ft_sort_filecmp_c); 
 }
