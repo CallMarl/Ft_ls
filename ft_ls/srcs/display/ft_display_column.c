47c9a0f858fbed69efc8494aef9b90c7bff82d27
Paul PRIKAZSKY <paul.prikazsky@gmail.com>
2018-05-15 14:48:17 +0200

    Fix largeur nombre de fichier par ligne si multiples

--- ft_ls/srcs/display/ft_display_column.c
+++ ft_ls/srcs/display/ft_display_column.c
@@ -6,7 +6,7 @@
 /*   By: pprikazs <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/14 16:53:15 by pprikazs          #+#    #+#             */
-/*   Updated: 2018/05/15 12:10:07 by pprikazs         ###   ########.fr       */
+/*   Updated: 2018/05/15 14:44:36 by pprikazs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,7 @@ static size_t		ft_display_getmaxlen(t_buff *buff)
 static size_t		ft_compt_nb_column(void)
 {
 	struct winsize ws;
+
 
 	if (ioctl(STDIN_FILENO, TIOCGWINSZ, &ws) == -1)
 		return (0);
@@ -77,12 +78,14 @@ static void			ft_display_column_aux(t_buff *buff, size_t max_len, \
 {
 	size_t			i;
 	size_t			y;
+	size_t			z;
 	t_file			*file;
 
 	y = 0;
 	i = 0;
 	while (i < buff->cr)
 	{
+		z = 0;
 		file = &((t_file *)buff->buff)[i];
 		if (file->name[0] != '.' || opt_a != 0)
 		{
@@ -93,11 +96,13 @@ static void			ft_display_column_aux(t_buff *buff, size_t max_len, \
 		if (y == nb_file)
 		{
 			ft_putchar('\n');
+			z = y;
 			y = 0;
 		}
 		i++;
 	}
-	ft_putchar('\n');
+	if (z != nb_file)
+		ft_putchar('\n');
 }
 
 /*
@@ -107,10 +112,26 @@ static void			ft_display_column_aux(t_buff *buff, size_t max_len, \
 extern int			ft_display_column(t_buff *buff, int opt_a)
 {
 	size_t			max_len;
+	size_t			tmp;
 	size_t			column;
+	size_t			nb_file;
 
 	max_len = ft_display_getmaxlen(buff);
 	column = ft_compt_nb_column();
-	ft_display_column_aux(buff, max_len, column / (max_len + 1), opt_a);
+	tmp = 2;
+	nb_file = 1;
+	if (opt_a == 0)
+		(buff->cr) -= 2;
+	while ((max_len + 1) * (buff->cr / nb_file) > column && tmp < buff->cr)
+	{
+		if (buff->cr % tmp == 0 && tmp < buff->cr)
+			nb_file = buff->cr / tmp;
+		tmp++;
+	}
+	if (nb_file == 1 || (max_len + 1) * (buff->cr / nb_file) > column)
+		nb_file= column / (max_len + 1);
+	if (opt_a == 0)
+		(buff->cr) += 2;
+	ft_display_column_aux(buff, max_len, nb_file, opt_a);
 	return (1);
 }
